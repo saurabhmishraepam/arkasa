182e85276d97a77bc65de711d175aab951bfe8fa
Gowtham Thurangi <Gowtham_Thurangi@epam.com>
2018-08-26 02:30:46 +0530

    added notification controller

---
+++ backend-app/src/main/java/com/epam/araksa/controllers/NotificationController.java
@@ -0,0 +1,16 @@
+package com.epam.araksa.controllers;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.epam.araksa.dto.EmployeeStatus;
+
+@RestController
+public class NotificationController {
+
+	@PutMapping("notification")
+	public String updateEmployeeStatus(EmployeeStatus employeeStatus) {
+		System.out.println("Employee status :"+employeeStatus);
+		return "Updated";
+	}
+}
